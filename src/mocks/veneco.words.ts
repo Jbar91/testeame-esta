38974f78c27be4dee495c83eb712da2094541517
Jbar91 <jibarlucea91@gmail.com>
2023-06-04 22:12:05 +0200

    Add veneco words type and mock

---
+++ src/mocks/veneco.words.ts
@@ -0,0 +1,26 @@
+import { VenecoWord } from "../models/types";
+
+export const words: VenecoWord[] = [
+  { word: "Marico el que lo lea", meaning: "jeje lo leiste" },
+  { word: "Aweboniao", meaning: "Persona que esta 'empana', tonta o lenta" },
+  {
+    word: "Vergacion",
+    meaning: "No se, tiene muchos usos pero normalmente es para exagerar algo",
+  },
+  {
+    word: "Bruja",
+    meaning: "Persona a la cual no se le puede fiar (la policia por ejemplo)",
+  },
+  { word: "Mamaguevo", meaning: "Gilipollas" },
+  { word: "Vergatario", meaning: "Preguntenle a Edu, el sabe" },
+  {
+    word: "Ladilla",
+    meaning: "Una persona muy necia o algo que da mucho fastidio",
+  },
+  { word: "Mardito", meaning: "Maldito, pero en maracucho" },
+  { word: "Becerro", meaning: "Una vaca pequeña o una persona insoportable" },
+  { word: "Chivo", meaning: "Una cabra" },
+  { word: "Enperifollao", meaning: "Estar muy bien vestido" },
+  { word: "Mamando y loco", meaning: "No tener dinero" },
+  { word: "Saperoco", meaning: "Desorden, desastre" },
+];
